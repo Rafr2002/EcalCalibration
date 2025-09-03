a3443c4e67f22d1162f9b7b2608b64dd7c3defdb
Rafr2002 <raf2002@users.noreply.github.com>
2025-09-03 16:35:39 -0400

    get_photon_values analyzer script

--- analyzer_scripts/get_photon_values.C
+++ analyzer_scripts/get_photon_values.C
@@ -15,7 +15,7 @@
 
 
 
-void Hit_based_analyze_pi0(const char *fname="10K_pi0_Ecal_cal.root") {
+void get_photon_values(const char *fname="10K_pi0_Ecal_cal.root") {
 
   // Open file and tree
   TFile *f = TFile::Open(fname);
@@ -83,27 +83,28 @@ T->SetBranchAddress("SDTrack.vEkin", &vTkin);
 //**Initialize Histograms
 
 //PDG Particle Ids
-TH1I *hPID  = new TH1I("hPID","Track PID ;Counts",300,-50,250);  
-TH1I *hMPID = new TH1I("hMPID","Parent PID ;Counts",300,-50,250);  
+TH1I *hPID  = new TH1I("hPID","Particle ID(photon=22) ;ID",20,10,30);  
+TH1I *hMPID = new TH1I("hMPID","Parent PID(#pi-0=111) ;ID",140,-20,120);  
 TH2I *hPIDvsMPID = new TH2I("hPIDvsMPID","Track PID vs Parent PID;Parent PID;Track PID",
-                             300,-50,250, 300,-50,250);
+170,-50,250, 170,-50,120);
 
 //Photon Kinematics                             
-TH1D *hPhE = new TH1D("hPhotonE","Photon Energy [GeV];Counts",200,0,7);
+TH1D *hPhE = new TH1D("hPhotonE","Photon Energy [GeV];Counts",50,0,7);
 
-TH1D *hXPi     = new TH1D("hXPi","X Spread of Pi0 Photons;x [m];Counts",200,-2,2);
-TH1D *hXNotPi  = new TH1D("hXNotPi","X Spread of Non-Pi0 Photons;x [m];Counts",200,-2,2);
-TH1D *hYPi     = new TH1D("hYPi","Y Spread of Pi0 Photons;y [m];Counts",200,-2,2);
-TH1D *hYNotPi  = new TH1D("hYNotPi","Y Spread of Non-Pi0 Photons;y [m];Counts",200,-2,2);
+TH1D *hXPi     = new TH1D("hXPi","X Spread of Pi0 Photons;x [m];Counts",50,-2,2);
+TH1D *hXNotPi  = new TH1D("hXNotPi","X Spread of Non-Pi0 Photons;x [m];Counts",50,-2,2);
+TH1D *hYPi     = new TH1D("hYPi","Y Spread of Pi0 Photons;y [m];Counts",50,-2,2);
+TH1D *hYNotPi  = new TH1D("hYNotPi","Y Spread of Non-Pi0 Photons;y [m];Counts",50,-2,2);
 
-TH2D *hXY = new TH2D("hXY","Photon hit positions;x [m];y [m]",200,-2,2,200,-2,2);
+TH2D *hXY = new TH2D("hXY","Photon hit positions;x [m];y [m]",100,-4,4,100,-2,2);
 
-TH1D *hTheta = new TH1D("hTheta","Photon polar angle #theta [deg];#theta [deg];Counts",180,0,180);
-TH1D *hPhi   = new TH1D("hPhi","Photon azimuthal angle #phi [deg];#phi [deg];Counts",180,-180,180);
+TH1D *hTheta = new TH1D("hTheta","Photon polar angle #theta [deg];#theta [deg];Counts",30,15,45);
+TH1D *hPhi   = new TH1D("hPhi","Photon azimuthal angle #phi [deg];#phi [deg];Counts",90,-45,45);
 
 
-TH1D *hPi0Mass = new TH1D("hPi0Mass","#pi^{0} invariant mass;M_{#gamma#gamma} [GeV];Counts",200,0,0.3);
-TH1D *hOpeningAng = new TH1D("hOpenAngle","Opening angle between photons;θ_{12} [deg];Counts",180,0,180);
+TH1D *hPi0Mass = new TH1D("hPi0Mass","#pi^{0} invariant mass;M_{#gamma#gamma} [GeV];Counts",10,0,0.15);
+// TH1D *hStrictPi0Mass = new TH1D("hPi0Mass","#pi^{0} invariant mass strictly of pairs that were produced by Pi0 ;M_{#gamma#gamma} [GeV];Counts",10,0,0.15);
+TH1D *hOpeningAng = new TH1D("hOpenAngle","Opening angle between photons;#theta_{12} [#deg];Counts",20,0,20);
 TH1D *hE1          = new TH1D("hE1","Photon 1 Energy;E1 [GeV];Counts",100,0,7);
 TH1D *hE2          = new TH1D("hE2","Photon 2 Energy;E2 [GeV];Counts",100,0,7);
 TH2D *hEvsE        = new TH2D("hEvsE","Photon 2 vs Photon 1 Energy;E1 [GeV];E2 [GeV]",100,0,7,100,0,7);
@@ -134,44 +135,57 @@ for(Long64_t ie=0; ie<nentries; ie++){
       hXPi->Fill(posx->at(j));
       hYPi->Fill(posy->at(j));
 
+      hXY->Fill(posx->at(j), posy->at(j));
+
       eventPhotons.push_back(ph);
     }
 
-    // Reconstruct invariant mass if >=2 pi0 photons
-     if(eventPhotons.size()>=2){
-
-      // take top-2 by energy
-      // std::sort(eventPhotons.begin(),eventPhotons.end(),
-      // [](const TLorentzVector&a,const TLorentzVector&b){return a.E()>b.E();});
-      
-      TLorentzVector ph1=eventPhotons[0];
-      TLorentzVector ph2=eventPhotons[1];
-
-      hE1->Fill(ph1.E());
-      hE2->Fill(ph2.E());
-      hEvsE->Fill(ph1.E(),ph2.E());
-      hEvsE->Fill(ph2.E(),ph1.E());
-
-      double mgg=(ph1+ph2).M();
-      hPi0Mass->Fill(mgg);
-
-      double opening=ph1.Vect().Angle(ph2.Vect())*180.0/TMath::Pi();
-      hOpeningAng->Fill(opening);
-    }
+    else if (PID->at(j) == 22) {
+        // photon but not from pi0
+        hXNotPi->Fill(posx->at(j));
+        hYNotPi->Fill(posy->at(j));
+      }
   }
+
+    // Reconstruct invariant mass if >=2 pi0 photons
+    if(eventPhotons.size()>=2){
+
+    // take top-2 by energy
+    // std::sort(eventPhotons.begin(),eventPhotons.end(),
+    // [](const TLorentzVector&a,const TLorentzVector&b){return a.E()>b.E();});
+    
+    TLorentzVector ph1=eventPhotons[0];
+    TLorentzVector ph2=eventPhotons[1];
+
+    hE1->Fill(ph1.E());
+    hE2->Fill(ph2.E());
+    hEvsE->Fill(ph1.E(),ph2.E());
+
+    // if (MPID[i]==111){
+    //   hStrictPi0Mass->Fill(ph1.E(),ph2.E());
+    // }
+
+    double mgg=(ph1+ph2).M();
+    hPi0Mass->Fill(mgg);
+
+    double opening=ph1.Vect().Angle(ph2.Vect())*180.0/TMath::Pi();
+    hOpeningAng->Fill(opening);
+  }
+}
 
 
   // Draw canvases
-  TCanvas *c1=new TCanvas("c1","PID Distributions",1000,600);
-  c1->Divide(2,2);
+  TCanvas *c1=new TCanvas("c1","Particle IDs(negative are antiparticles)",1000,600);
+  // c1->Divide(2,2);
+  c1->Divide(2,1);
   c1->cd(1); hPID->Draw();
-  c1->cd(2); hMPID->Draw();
-  c1->cd(3); hPIDvsMPID->Draw("COLZ");
+  c1->cd(2)->GetLogy(); hMPID->Draw();
+  // c1->cd(3); hPIDvsMPID->Draw("COLZ");
 
   TCanvas *c2=new TCanvas("c2","Photon Kinematics",1000,600);
   c2->Divide(2,2);
   c2->cd(1); hPhE->Draw();
-  c2->cd(2); hTheta->Draw();
+  c2->cd(2)->SetLogy(); hTheta->Draw();
   c2->cd(3); hPhi->Draw();
   c2->cd(4); hXY->Draw("COLZ");
 
@@ -186,5 +200,5 @@ for(Long64_t ie=0; ie<nentries; ie++){
   leg->Draw();
   c3->cd(2); hEvsE->Draw("COLZ");
   c3->cd(3); hPi0Mass->Draw();
-  c3->cd(4); hOpeningAng->Draw();
+  c3->cd(4)->SetLogy(); hOpeningAng->Draw();
 }
